c96d3f6773a72203a77854f74a1a514ecd0f4cce
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-09-17 12:04:04 +0530

    Feat(core): Add Profile sub pages

---
+++ frontend/src/pages/core/standardProfile/Overview.tsx
@@ -0,0 +1,5 @@
+export const Overview = () => {
+  return (
+    <div>Overview Page</div>
+  )
+}
--- frontend/src/utils/TabsData.tsx
+++ frontend/src/utils/TabsData.tsx
@@ -1,22 +1,27 @@
+import { Experience } from "@/pages/core/standardProfile/Experience";
+import { Featured } from "@/pages/core/standardProfile/Featured";
+import { Overview } from "@/pages/core/standardProfile/Overview";
+import { Projects } from "@/pages/core/standardProfile/Projects";
+
 export const standardTabsData = [
 	{
 		title: "Overview",
-		content: "Overview",
+		content: <Overview/>
 	},
 	{
 		title: "Projects",
-		content: "Projects",
+		content: <Projects/>,
 	},
 	{
 		title: "Experience",
-		content: "Experience",
+		content: <Experience/>,
 	},
 	{
 		title: "Featured",
-		content: "Featured",
-	},
-	{
-		title: "Posts",
-		content: "Posts",
+		content: <Featured/>,
 	},
 ];
+// {
+//     title: "Posts",
+//     content: "Posts",
+// },
