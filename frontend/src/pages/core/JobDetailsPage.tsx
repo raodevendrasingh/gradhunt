c9bd02f37164280a0298405bee0c4de9b5163669
Devendra Singh Rao <devthedeveloperguy@gmail.com>
2024-10-20 22:53:30 +0530

    Fix(core): Updated job details page ui

--- frontend/src/pages/core/JobDetailsPage.tsx
+++ frontend/src/pages/core/JobDetailsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, ReactNode } from "react";
 import { IoPaperPlaneOutline } from "react-icons/io5";
 import { useFetchJobDetails } from "@/hooks/useFetchJobDetails";
 import { useParams } from "react-router-dom";
@@ -18,9 +18,10 @@ import JobCardMenu from "@/components/layouts/JobCardMenu";
 import { LiaMoneyBillWaveAltSolid } from "react-icons/lia";
 import { HiOutlineUsers } from "react-icons/hi2";
 import { formatDate } from "@/utils/FormatDate";
+import { timesAgo } from "@/utils/DaysAgo";
+import { LoadingBlock } from "@/components/ui/LoadingBlock";
 
 export const JobDetailsPage: React.FC = () => {
-	const [scrolled, setScrolled] = useState(false);
 	const contentRef = useRef<HTMLDivElement>(null);
 	const { jobId } = useParams<{ jobId: string }>();
 
@@ -28,45 +29,14 @@ export const JobDetailsPage: React.FC = () => {
 		return <NotFound />;
 	}
 
-	const checkScroll = () => {
-		if (contentRef.current) {
-			const isScrolled = contentRef.current.scrollTop > 0;
-			setScrolled(isScrolled);
-		}
-	};
-
-	useEffect(() => {
-		checkScroll();
-
-		const observer = new MutationObserver(() => {
-			checkScroll();
-		});
-
-		if (contentRef.current) {
-			observer.observe(contentRef.current, { childList: true, subtree: true });
-		}
-
-		const handleScroll = () => {
-			checkScroll();
-		};
-
-		const currentRef = contentRef.current;
-		if (currentRef) {
-			currentRef.addEventListener("scroll", handleScroll, { passive: true });
-		}
-
-		return () => {
-			if (currentRef) {
-				currentRef.removeEventListener("scroll", handleScroll);
-			}
-			observer.disconnect();
-		};
-	}, []);
-
 	const { data, isLoading } = useFetchJobDetails({ jobId });
 
-	if (!data) {
-		return <div>Loading...</div>;
+	if (!data || isLoading) {
+		return (
+			<div className="w-full lg2:w-[70%] h-screen flex items-center justify-center border-r">
+				<LoadingBlock size={36} color="#475569" />
+			</div>
+		);
 	}
 
 	const handleBackClick = () => {
@@ -77,51 +47,88 @@ export const JobDetailsPage: React.FC = () => {
 	return (
 		<div className="flex h-full">
 			<div className="relative flex flex-col w-full lg2:w-[70%] overflow-hidden border-r">
-				<ScrollableHeader
-					jobId={data.jobId}
-					title={data.jobTitle}
-					isScrolled={scrolled}
-					onBackClick={handleBackClick}
-				/>
-				<div ref={contentRef} className="flex-1 overflow-y-auto scrollbar-hide">
-					<div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mb-6 p-5 bg-slate-100 rounded-b-2xl">
-						<InfoItem icon={<LuMapPin size={20} />} text={data.jobLocation} />
-						<InfoItem icon={<LuBriefcase size={20} />} text={data.jobType} />
-						<InfoItem icon={<LuMapPin size={20} />} text={data.workType} />
-						<InfoItem
-							icon={<LuClock size={20} />}
-							text={`${data.experience} years`}
-						/>
-						<InfoItem
-							icon={<LiaMoneyBillWaveAltSolid size={20} />}
-							text={
-								data.currency +
-								" " +
-								data.lowestSalary +
-								" - " +
-								data.highestSalary
-							}
-						/>
-						<InfoItem
-							icon={<HiOutlineUsers size={20} />}
-							text={`Openings: ${data.openings}`}
-						/>
-						<InfoItem
-							icon={<LuCalendar size={20} />}
-							text={`Apply by: ${formatDate(data.applicationDeadline)}`}
-						/>
+				<div className="sticky top-0 left-0 right-0 z-40 bg-slate-50 px-4 pt-4">
+					<div className="flex justify-between items-center mb-4">
+						<div className="flex items-center justify-start">
+							<button
+								onClick={handleBackClick}
+								className="p-1.5 rounded-full hover:bg-gray-100 transition-colors"
+							>
+								<BsArrowLeftCircle
+									className="text-gray-600 hover:text-gray-800"
+									size={24}
+								/>
+							</button>
+							<div className="flex items-center pl-2">
+								<h1 className="text-xl font-bold text-gray-800 pr-4 truncate">
+									{data.jobTitle}
+								</h1>
+								<div className="flex items-center gap-2 ">
+									<span className="px-2.5 py-1 bg-white rounded-md text-gray-600 text-sm border border-gray-100">
+										JobID: {data.jobId}
+									</span>
+								</div>
+							</div>
+						</div>
+						<JobCardMenu editUrl="edit" />
 					</div>
-					<div className="p-5">
-						<h4 className="font-medium mb-2 text-gray-800">
-							Applicants: {data.applicants}
-						</h4>
+				</div>
+				<div ref={contentRef} className="flex-1 overflow-y-auto scrollbar-hide">
+					<div className="p-6 bg-gradient-to-b from-slate-50 to-white">
+						<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-sm">
+							<InfoItem
+								icon={<LuClock size={20} />}
+								title="Posted"
+								text={timesAgo(data.postedDate)}
+							/>
+							<InfoItem
+								icon={<HiOutlineUsers size={20} />}
+								title="Applicants"
+								text={data.applicants.toString()}
+							/>
+							<InfoItem
+								icon={<HiOutlineUsers size={20} />}
+								title="Openings"
+								text={data.openings.toString()}
+							/>
+							<InfoItem
+								icon={<LuMapPin size={20} />}
+								title="Location"
+								text={data.jobLocation}
+							/>
+							<InfoItem
+								icon={<LuBriefcase size={20} />}
+								title="Job Type"
+								text={data.jobType}
+							/>
+							<InfoItem
+								icon={<LuMapPin size={20} />}
+								title="Work Type"
+								text={data.workType}
+							/>
+							<InfoItem
+								icon={<LuClock size={20} />}
+								title="Experience"
+								text={`${data.experience} years`}
+							/>
+							<InfoItem
+								icon={<LiaMoneyBillWaveAltSolid size={20} />}
+								title="Salary"
+								text={`${data.currency} ${data.lowestSalary} - ${data.highestSalary}`}
+							/>
+							<InfoItem
+								icon={<LuCalendar size={20} />}
+								title="Application Deadline"
+								text={formatDate(data.applicationDeadline)}
+							/>
+						</div>
 					</div>
 
-					<div className="p-5">
-						<h2 className="text-xl font-semibold mb-2 text-gray-800">
+					<div className="p-6 bg-slate-50">
+						<h2 className="text-xl font-semibold mb-4 text-gray-800">
 							Required Skills
 						</h2>
-						<div className="flex flex-wrap gap-2">
+						<div className="flex flex-wrap gap-3">
 							{isLoading
 								? [...Array(8)].map((_, index) => (
 										<div
@@ -132,12 +139,12 @@ export const JobDetailsPage: React.FC = () => {
 								: data.requiredSkills.map((skill) => (
 										<div
 											key={skill.value}
-											className="flex items-center justify-center text-sm px-2.5 py-[3px] bg-slate-50 text-gray-700 rounded-full border m-1 cursor-context-menu"
+											className="flex items-center justify-center text-sm px-4 py-1.5 bg-white text-gray-700 rounded-full border border-gray-200 hover:border-gray-300 transition-colors duration-200"
 										>
 											<img
 												src={skill.image}
 												alt={skill.label}
-												className="size-4 mr-2"
+												className="w-4 h-4 mr-2"
 											/>
 											{skill.label}
 										</div>
@@ -145,38 +152,65 @@ export const JobDetailsPage: React.FC = () => {
 						</div>
 					</div>
 
-					<div className="p-5">
-						<h2 className="text-xl font-semibold mb-2 text-gray-800">
+					<div className="p-6 bg-white">
+						<h2 className="text-xl font-semibold mb-4 text-gray-800">
 							Job Description
 						</h2>
 						<div
 							dangerouslySetInnerHTML={{ __html: data.jobDescription }}
 							className="text-gray-700 prose max-w-none"
-							style={{ lineHeight: "1.5" }}
+							style={{ lineHeight: "1.6" }}
 						/>
 					</div>
-
-					<div className="flex items-center justify-between border-t p-5 space-x-4">
-						{data.applyWithUs && (
-							<Button
-								variant="secondary"
-								className="flex w-full items-center gap-2 rounded-lg py-2.5"
-							>
-								Apply Now
-								<IoPaperPlaneOutline size={18} />
-							</Button>
-						)}
-						{data.applyLink && (
-							<Button
-								variant="primary"
-								className="flex w-full items-center gap-2 rounded-lg py-2.5"
-								onClick={() =>
-									window.open(data.applyLink, "_blank", "noopener,noreferrer")
-								}
-							>
-								Apply on Company Site
-								<LuArrowUpRight size={18} />
-							</Button>
+					<div className=" sticky bottom-0 flex items-center justify-end bg-white border-t p-3 space-x-4">
+						{data.applyWithUs && data.applyLink ? (
+							<>
+								<Button
+									variant="primary"
+									className="flex w-full items-center gap-2 rounded-lg py-2.5"
+								>
+									Easy Apply
+									<IoPaperPlaneOutline size={18} />
+								</Button>
+								<Button
+									variant="secondary"
+									className="flex w-full items-center gap-2 rounded-lg py-2.5"
+									onClick={() =>
+										window.open(data.applyLink, "_blank", "noopener,noreferrer")
+									}
+								>
+									Apply
+									<LuArrowUpRight size={18} />
+								</Button>
+							</>
+						) : (
+							<>
+								{data.applyWithUs && (
+									<Button
+										variant="primary"
+										className="flex w-44 items-center gap-2 rounded-lg py-2.5"
+									>
+										Easy Apply
+										<IoPaperPlaneOutline size={18} />
+									</Button>
+								)}
+								{data.applyLink && (
+									<Button
+										variant="secondary"
+										className="flex w-44 items-center gap-2 rounded-lg py-2.5"
+										onClick={() =>
+											window.open(
+												data.applyLink,
+												"_blank",
+												"noopener,noreferrer"
+											)
+										}
+									>
+										Apply
+										<LuArrowUpRight size={18} />
+									</Button>
+								)}
+							</>
 						)}
 					</div>
 				</div>
@@ -185,90 +219,18 @@ export const JobDetailsPage: React.FC = () => {
 	);
 };
 
-const InfoItem: React.FC<{ icon: React.ReactNode; text: string }> = ({
-	icon,
-	text,
-}) => (
-	<div className="flex items-center text-gray-800">
-		<span className="text-gray-500 mr-2">{icon}</span>
-		<span>{text}</span>
+const InfoItem: React.FC<{
+	icon: React.ReactNode;
+	title: string;
+	text: string;
+}> = ({ icon, title, text }) => (
+	<div className="flex items-start p-2.5 bg-white rounded-lg border border-gray-100 hover:border-gray-200 transition-colors duration-200">
+		<div className="flex items-center justify-center w-8 h-8 bg-slate-50 rounded-full mr-4 mt-1">
+			<span className="text-gray-600">{icon}</span>
+		</div>
+		<div className="flex flex-col">
+			<span className="text-sm font-medium text-gray-500 mb-1">{title}</span>
+			<span className="text-gray-800 font-medium">{text}</span>
+		</div>
 	</div>
 );
-
-interface ScrollableHeaderProps {
-	jobId: string;
-	title: string;
-	isScrolled: boolean;
-	onBackClick: () => void;
-}
-
-export const ScrollableHeader: React.FC<ScrollableHeaderProps> = ({
-	jobId,
-	title,
-	isScrolled,
-	onBackClick,
-}) => {
-	const [animate, setAnimate] = useState(false);
-
-	useEffect(() => {
-		const timer = setTimeout(() => setAnimate(true), 100);
-		return () => clearTimeout(timer);
-	}, []);
-
-	const transition = { duration: 0, ease: "easeInOut" };
-
-	return (
-		<motion.div
-			className={clsx(
-				"sticky top-0 left-0 right-0 z-40 transition-all",
-				isScrolled
-					? "bg-white bg-opacity-20 backdrop-blur-md border-b"
-					: "bg-slate-200"
-			)}
-			animate={{ height: animate ? (isScrolled ? 60 : 130) : 130 }}
-			transition={transition}
-		>
-			<div className="flex flex-col content-center h-full w-full py-4">
-				<div className="flex justify-between items-center px-4">
-					<BsArrowLeftCircle
-						className="cursor-pointer text-gray-600"
-						size={24}
-						onClick={onBackClick}
-					/>
-					<JobCardMenu editUrl="edit" />
-				</div>
-
-				<div className="flex-grow flex items-center px-4">
-					<AnimatePresence initial={false} mode="wait">
-						{isScrolled ? (
-							<motion.h1
-								key="scrolled-title"
-								initial={{ opacity: 0, y: 30 }}
-								animate={{ opacity: 1, y: -30 }}
-								exit={{ opacity: 0, y: 20 }}
-								transition={transition}
-								className="text-xl ml-12 font-bold text-gray-800 truncate w-fit"
-							>
-								{title}
-							</motion.h1>
-						) : (
-							<motion.h1
-								key="full-title"
-								initial={{ opacity: 0, y: 15 }}
-								animate={{ opacity: 1, y: 15 }}
-								exit={{ opacity: 0, y: 15 }}
-								transition={transition}
-								className="text-2xl font-bold text-gray-800 truncate flex-grow py-1"
-							>
-								{title}
-								<span className="ml-2 mb-2 py-1 px-2 rounded-lg bg-slate-50 text-gray-600 text-sm font-normal border border-slate-50">
-									JobID: {jobId}
-								</span>
-							</motion.h1>
-						)}
-					</AnimatePresence>
-				</div>
-			</div>
-		</motion.div>
-	);
-};
